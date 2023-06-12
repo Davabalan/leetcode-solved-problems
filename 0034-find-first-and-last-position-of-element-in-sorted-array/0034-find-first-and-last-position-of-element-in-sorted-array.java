ecde0bdfb232c9765eb0ff9fe958607855424d33
Davabalan <balandava@gmail.com>
2023-06-12 21:09:41 +0530

    Time: 0 ms (100%), Space: 44.7 MB (78.93%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,35 +1,31 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-               int start =0;
-        int end  = nums.length-1;
+        int low = 0;
+        int high = nums.length - 1;
         int mid;
         int[] output = {-1,-1};
-        while(start<=end){
-            mid = start+(end-start)/2;
+        while(low <= high) {
+            mid = low + (high - low)/2;
             if(nums[mid] == target){
-                output[0]=mid;
-                end =mid-1;
-            }
-            else if(nums[mid]<target){
-                start =mid+1;
-            }
-            else{
-                end =mid-1;
+                output[0] = mid;
+                high = mid - 1;
+            } else if(nums[mid]<target) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
             }
         }
-        start =0;
-        end= nums.length-1;
-        while(start<=end){
-            mid = start+(end-start)/2;
-            if(nums[mid] == target){
-                output[1]=mid;
-                start =mid+1;
-            }
-            else if(nums[mid]<target){
-                start =mid+1;
-            }
-            else{
-                end =mid-1;
+        low = 0;
+        high = nums.length - 1;
+        while(low <= high) {
+            mid = low + (high - low) / 2;
+            if(nums[mid] == target) {
+                output[1] = mid;
+                low = mid + 1;
+            } else if(nums[mid] < target) {
+                low = mid + 1;
+            } else {
+                high = mid-1;
             }
         }
         return output; 
