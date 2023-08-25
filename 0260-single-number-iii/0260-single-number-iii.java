9426984d8a13eea952a17974ca1008e9dccc3cd7
Davabalan <balandava@gmail.com>
2023-08-25 20:51:51 +0530

    Time: 1 ms (99.73%), Space: 44.3 MB (72.45%) - LeetHub

--- 0260-single-number-iii/0260-single-number-iii.java
+++ 0260-single-number-iii/0260-single-number-iii.java
@@ -1,23 +1,15 @@
 class Solution {
     public int[] singleNumber(int[] nums) {
-        int xorResult = 0, x = 0, y = 0;
-        int[] twoUniqueElements = new int[2];
-        for( int i = 0; i < nums.length; i++) {
-            xorResult = xorResult ^ nums[i];
+        int xorResult = 0, lsb = 0, x = 0, y = 0;
+        int [] twoUniqueElements = new int[2];
+        for(int i : nums) {
+            xorResult = xorResult ^ i;
         }
-
-        //Find Last set Bit
-        int lsb = xorResult & ~(xorResult - 1);
-        
-        for (int i : nums)
-        {
-            // Seggregate set bits from the array
-            if ((i & lsb) != 0) {
+        lsb = xorResult & ~(xorResult - 1);
+        for(int i : nums) {
+            if((lsb & i) != 0) {
                 x = x ^ i;
-            }
- 
-            // elements that don't have the bit set
-            else {
+            } else {
                 y = y ^ i;
             }
         }
